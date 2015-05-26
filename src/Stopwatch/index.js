f4f71ba73cee5d18af3a0871884272c53a936c1f
Naman Goel <naman34@gmail.com>
2015-05-27 01:01:22 +0200

    stopwatch complete

--- src/Stopwatch/index.js
+++ src/Stopwatch/index.js
@@ -8,8 +8,23 @@ class Stopwatch extends React.Component {
       clockRunning: false
     }
   }
+  updateTime(){
+    if(this.state.clockRunning){
+      var currentTime = Date.now()
+      var newElapsedTime = this.elapsedBeforeStart + (currentTime - this.lastStartedAt) / 1000
+      this.setState({elapsedTime: Math.round(newElapsedTime)})
+      setTimeout(this.updateTime.bind(this), 1000)
+    }
+  }
   startClock(){
+    this.lastStartedAt = Date.now()
+    this.elapsedBeforeStart = this.state.elapsedTime
     this.setState({clockRunning: true})
+  }
+  componentDidUpdate(prevProps, prevState){
+    if(!prevState.clockRunning && this.state.clockRunning){
+      this.updateTime()
+    }
   }
   pauseClock(){
     this.setState({clockRunning: false})
@@ -21,12 +36,12 @@ class Stopwatch extends React.Component {
     return (
       <div className="stop-watch">
         <span className="time-display">
-          {this.props.seconds}
+          {this.props.seconds + this.state.elapsedTime}
         </span>
         <br />
 
         { this.state.clockRunning ?
-            <button onClick={this.pauseClock.bind(this)}>Stop</button>
+            <button onClick={this.pauseClock.bind(this)}>Pause</button>
           :
             <button onClick={this.startClock.bind(this)}>Start</button>
         }
